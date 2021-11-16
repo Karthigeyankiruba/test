8fc0a7421d34ef85e05314b8c72c1d9383061e4b
Karthigeyan K <94433157+Karthigeyankiruba@users.noreply.github.com>
2021-11-16 20:52:55 +0530

    task
    today's task given by venkat sir

---
+++ task1.js
@@ -0,0 +1,26 @@
+var input = [{
+    "MemorySummary":[{
+    'maximumCapacity' : 2048"MBytes",
+    'maximumMemoryModuleSize' : 4096MBytes,
+    'memorySlots' : 4,
+    'ErrorCrecction' : 'NamedNodeMap',
+    'DRAMFrequency': 199.0mhz,
+    'memoryTimigs' : 3-3-3-8(CLRCD-RP-RAS),
+}],{
+    DeviceLocator :[{
+        "manufaturer" : "hyundaiElectronics",
+        "partNumber" : "HYMD264 646D8J-D43",
+        "serialNumber" : FFFF7240,
+        "capacity" : 512MBYES,
+        "memoryType" : DDR (PC3200),
+        "speed" : 200MHz,
+        "suppoertedFrequencies" : 133mhz,166mhz,
+        "memoryTimings" : 2-2-2-6-0 at 133mhz,
+        "memoryTimings1" : 2-3-3-7-0 at 166mhz,
+        "memoryTimings2" : 3-3-3-8-0 at 200 mhz,
+        "dataWidth" : 64 ,"bit",
+        "manufacturingDate" :  2005,
+        "EPP SPD Support" : "no",
+        "XMP SPD Support" : "no"
+    }]
+}]
